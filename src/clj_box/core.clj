5b37ee0876917c621ff4fac22b782dc84b1206c0
Jamie Atkinson <jabbslad@gmail.com>
2013-08-28 23:49:07 +0100

    cleanup body regex parsing and make it more flexible

--- src/clj_box/core.clj
+++ src/clj_box/core.clj
@@ -5,19 +5,27 @@
 (def ^:dynamic *cookie-store* (clj-http.cookies/cookie-store))
 (def ^:dynamic *signin-url* "https://live.xbox.com/en-US/Friends")
 
-(defn get-page-data [pattern body]
-	(first (rest (re-find pattern body))))
+(defn get-page-data [body patterns]
+	(reduce #(assoc %1 (first %2) (last %2)) {} (for [[k v] patterns] [k (first (rest (re-find v body)))])))
+
+(defn- request [method url & [opts]]
+  (client/request
+    (merge {:method method :url url :cookie-store *cookie-store*} opts)))
+
+(defn- check-signin [username password]
+	(let [opts (get-page-data (:body (request :get *signin-url*)) {:url #"urlPost:'(.+?)'" :PPFT #"PPFT.*value=\"(.+?)\""})]
+		(request :post (:url opts)
+			{:form-params (merge opts {:login username
+									   :passwd password
+									   :type 11
+									   :LoginOptions 3
+									   :PPSX "Passp"})})))
+
+(defn- set-cookies [form-data])
 
 (defn login 
 	"Take a username and password and logs the user in"
 	[username password]
-	(let [response (client/get *signin-url* {:cookie-store *cookie-store*}) body (:body response)]
-		(client/post (get-page-data #"urlPost:'(.+?)'" body) 
-			{:cookie-store *cookie-store* :form-params {:login username
-						   :passwd password
-						   :type 11
-						   :LoginOptions 3
-						   :PPSX "Passp"
-						   :PPFT (get-page-data #"PPFT.*value=\"(.+?)\"" body)}})))
+	(check-signin username password))
 
 
